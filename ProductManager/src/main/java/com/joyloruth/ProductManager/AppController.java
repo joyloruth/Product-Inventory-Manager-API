5b8e15ebe452a2cbfe5d502889db015ea2fa80e5
Joy LoRuth <joyloruth@gmail.com>
2021-07-05 22:39:42 -0500

    update

--- ProductManager/src/main/java/com/joyloruth/ProductManager/AppController.java
+++ ProductManager/src/main/java/com/joyloruth/ProductManager/AppController.java
@@ -23,23 +23,41 @@ public class AppController {
 	@Autowired
 	private ProductService service; 
 	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		//Product addProduct =  new Product();
+		//model.addAttribute("addProduct", addProduct);
+		return "home";
+	}
 	
-	@RequestMapping("/")
+	@RequestMapping("/viewInventory")
 	public String viewHomePage(Model model) {
 		List<Product> listProducts = service.listAll();
 		model.addAttribute("listProducts", listProducts);
-		return "index";
+		return "viewInventory";
 	}
 	
-	@RequestMapping("/new")
+	@RequestMapping("/addProduct")
 	public String addProduct(Model model) {
 		Product addProduct =  new Product();
 		model.addAttribute("addProduct", addProduct);
-		return "new_page";
+		return "addProduct";
 	}
 	
+	@RequestMapping("/editProduct")
+	public String editProduct(Model model) {
+		Product editProduct =  new Product();
+		model.addAttribute("editProduct", editProduct);
+		return "editProduct";
+	}
 	
-	
+	@RequestMapping("/saveProduct")
+	public String saveProduct(Model model) {
+		Product saveProduct =  new Product();
+		model.addAttribute("saveProduct", saveProduct);
+		return "saveProduct";
+	}
+
 	
 	
 
